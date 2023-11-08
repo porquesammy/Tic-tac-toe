3819769604c0e7c6741fa4114c3041534725cb2f
porqueSammy <42855916+porquesammy@users.noreply.github.com>
2023-11-07 22:10:45 -0800

    Add mouseover player.symbol for move preview

--- js/main.js
+++ js/main.js
@@ -57,3 +57,26 @@ window.addEventListener("click", function (event) {
 restartGameBtn.addEventListener("click", () => {
   newGame();
 });
+
+//------------todo
+// onmouseover change svg to "x" if the img is "empty.svg"
+
+const gridSquares = document.querySelectorAll(".gameGridContainer__square");
+gridSquares.forEach((square) => {
+  square.addEventListener("mouseenter", (event) => {
+    if (event.target.childNodes[3].attributes[3].textContent === "empty") {
+      square.addEventListener("mouseleave", (e) => {
+        square.querySelector("img").replaceWith(ImgElementCreator("empty"));
+      });
+      square.querySelector("img").replaceWith(ImgElementCreator("x"));
+    }
+  });
+});
+
+// on click if empty humanMove()
+// -- add x to square and x to the array board
+// --checkWinner()
+// call makeCpuMove (maybe add a slight delay/make sure player can only go on his/her turn)
+// -- add o to square and o to the array board
+// --checkWinner()
+// if winner is declared add a point for winner
